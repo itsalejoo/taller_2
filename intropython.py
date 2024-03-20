51bf29d90e95095eb4dc0164f7bcdf758f40965f
alejandro sanchez <kealsansab@gmail.com>
2024-03-20 15:17:30 -0500

    variables

--- intropython.py
+++ intropython.py
@@ -2,3 +2,17 @@
 #todo lo que va despues es ignorado por el interpréte de python
 
 
+#variable: espacio de memoria, con nombre, donde guardo valores
+#los nombres de variables deben ser cortos, descriptivos, NO TENER ESPACIOS
+#en blanco, ni caracteres especiales, no deben empezar por un número
+
+#descriptivo significa que representa la categoria del dato que quiero guardar
+#en python las variables pueden contener cualquier dato de tipo primitvo
+#numeros (enteros, reales), cadena de carácteres(string),booleanos(true,false)
+#carácteres (letras)
+variable=1
+variable="juventud divino tesoro, te vas para no volver"
+variable=True
+variable="a"
+variable=3.1414926535
+#para asignar un valor a la variable se usa el operador = 
