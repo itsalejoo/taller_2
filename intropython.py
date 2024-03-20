478379757f7a17d9310b26db2262ff1788c799e8
alejandro sanchez <kealsansab@gmail.com>
2024-03-20 15:24:03 -0500

    avance

--- intropython.py
+++ intropython.py
@@ -16,3 +16,18 @@ variable=True
 variable="a"
 variable=3.1414926535
 #para asignar un valor a la variable se usa el operador = 
+
+#operadores: mecanismo para obtener un dato apartir de otros datos.
+#los datos que intervienen se llaman aperandos.
+
+#Aritmeticos: +,*,-,/,%
+#de comparación: retornan true o false. <,> >=, <= !=
+#los de lógica Boleana: Or, and. retornan T o F de acuerdo a una tabla de verdad
+#los operandos siempre son boleanos (true o false)
+
+a=True
+b=False
+
+print(a and b)
+#los operados boleanos y de comparción son muy utilizados al
+#definir condiciones
